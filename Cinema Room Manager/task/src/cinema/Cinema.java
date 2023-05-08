94839c273f9299aa277c08e8dae41e79e683b341
Sergey Goppikov <grey2k@gmail.com>
2023-05-08 22:55:38 +0300

    2/5

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -2,7 +2,12 @@ package cinema;
 
 public class Cinema {
 
+    final private static int ROWS = 7;
+    final private static int SEATS = 8;
     public static void main(String[] args) {
         // Write your code here
+        CinemaArea area = new CinemaArea(ROWS, SEATS);
+
+        area.print();
     }
 }
